841a0df51536f767fa172f076c6ab083f9b4545b
Arley Wilches Marcelo <arley.wilches@gmail.com>
2014-02-10 02:11:11 -0500

    path default

--- publishing/wsgi.py
+++ publishing/wsgi.py
@@ -8,7 +8,18 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+
+import os
+import sys
+from os.path import abspath, dirname, join
+
+ROOT_PATH = abspath(dirname(__file__))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "publishing.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "publishing.settings"
+
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
 
 import djcelery
 djcelery.setup_loader()
