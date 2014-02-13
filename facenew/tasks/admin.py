5e13df173f22c6a89b80cd500a3f2e08c3544f1b
Arley Wilches Marcelo <arley.wilches@gmail.com>
2014-02-12 23:55:04 -0500

    user crontab task

--- facenew/tasks/admin.py
+++ facenew/tasks/admin.py
@@ -23,7 +23,7 @@ class UserCrontabScheduleAdmin(admin.ModelAdmin):
     def has_edit_permission(self, request):
         return False
 
-    def has_delete_permission(self, request):
+    def has_delete_permission(self):
         return False
 
     pass
