e7184209436f2332dd386afa3c092810e3079228
Arley Wilches Marcelo <arley.wilches@gmail.com>
2014-02-14 16:46:24 -0500

    push whatsapp

--- facenew/tasks/tasks.py
+++ facenew/tasks/tasks.py
@@ -33,7 +33,7 @@ from django.db import close_connection
 from django.db import close_old_connections
 
 
-Debugger.enabled = False
+Debugger.enabled = True
 
 import subprocess
 import facebook
@@ -91,16 +91,16 @@ def message_whatsapp(account, cron_id):
     password = base64.b64decode(bytes(account.password.encode('utf-8')))
     phone_number = account.phone
     messages = Message.objects.filter(date__gte=datetime.date.today(), crontab=cron_id, type_message='whatsapp', enabled=True)
-    for i in range(15):
-        time.sleep(4)
-        for message in messages:
-            phone = Telephone.objects.select_for_update(
-                updated=True, exists=True, last_seen__year=datetime.datetime.now().year).exclude(
-                pk__in=MessagesTelephone.objects.filter(message=message, sended=True).values_list('phone', flat=True)
-            ).first()
-            MessagesTelephone.objects.create(phone=phone, message=message, sended_at=datetime.datetime.now(), sended=True)
-            wa = WhatsappEchoClient(phone, message.message.encode('utf-8'))
-            wa.login(phone_number, password)
+    # for i in range(15):
+    #     time.sleep(4)
+    for message in messages:
+        phone = Telephone.objects.select_for_update(
+            updated=True, exists=True, last_seen__year=datetime.datetime.now().year).exclude(
+            pk__in=MessagesTelephone.objects.filter(message=message, sended=True).values_list('phone', flat=True)
+        ).first()
+        MessagesTelephone.objects.create(phone=phone, message=message, sended_at=datetime.datetime.now(), sended=True)
+        wa = WhatsappEchoClient(phone.phone, message.message.encode('utf-8'))
+        wa.login(phone_number, password)
 
 
 
