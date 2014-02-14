32e5be5348b4e81971eba3643c56b1d92c8670c9
arleincho <arley.wilches@gmail.com>
2014-02-13 19:21:32 -0500

    time zone django

--- facenew/settings.py
+++ facenew/settings.py
@@ -102,7 +102,8 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 TIME_ZONE = 'UTC'
-CELERY_TIMEZONE='America/Bogota'
+TIME_ZONE = 'America/Bogota'
+CELERY_TIMEZONE = TIME_ZONE
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
