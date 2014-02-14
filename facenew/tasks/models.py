cb4155e78ed5b51eb54dc7deb4782827a4384243
Arley Wilches Marcelo <arley.wilches@gmail.com>
2014-02-14 15:45:57 -0500

    push whatsapp

--- facenew/tasks/models.py
+++ facenew/tasks/models.py
@@ -28,7 +28,7 @@ class Message(models.Model):
     )
 
     def __unicode__(self):
-        return '<Message: caption={0.caption}>'.format(self)
+        return '<Message: caption={0}>'.format(self.caption.encode('utf-8'))
 
 
 class UserCrontabSchedule(models.Model):
