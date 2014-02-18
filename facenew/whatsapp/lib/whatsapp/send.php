0aa11a90bdd6f0d2fbb20ca8cf6ee0afba78c16d
Arley Wilches Marcelo <arley.wilches@gmail.com>
2014-02-17 19:31:37 -0500

    php send whatsapp

--- facenew/whatsapp/lib/whatsapp/send.php
+++ facenew/whatsapp/lib/whatsapp/send.php
@@ -3,7 +3,7 @@ set_time_limit(10);
 require_once 'whatsprot.class.php';
 require_once 'contacts.php';
 
-$debug = true;
+$debug = false;
 
 // $username = "573123846473";
 // $identity = "356971040682553";
@@ -24,7 +24,7 @@ $id = $argv[6];
 function onMessageReceivedServer($phone, $from, $id, $type, $t){
 	$conn_string = "host=localhost port=5432 dbname=alquiler_perfil user=Dj4ngoU53rD4t4b4s3 password=Fvnja32QpxEZ5ppJYPmfP8umKKJGT2wH";
 	$db = pg_connect($conn_string);
-	$data = array('id' => 2);
+	$data = array('id' => $id);
 	$res = pg_update($db, 'whatsapp_messagesphonewhatsapp', array('message_whatsapp_id' => $id, 'sended_at' => date("Y-m-d H:i:s")), $data);
 }
 
