c1ed135ca87d45d0f5a39ae2b8d1a12c38aedba0
Arley Wilches Marcelo <arley.wilches@gmail.com>
2014-02-18 09:26:50 -0500

    php send whatsapp

--- facenew/tasks/tasks.py
+++ facenew/tasks/tasks.py
@@ -108,7 +108,7 @@ def message_whatsapp(account, message):
             script, account['phone_number'], '', account['password'], phone.phone, message.message.encode('utf-8'), message_phone_whatsapp.id, image)
 
         subprocess.call(["/usr/bin/php5 {0} {1} '{2}' {3} {4} '{5}' {6} '{7}'".format(
-            script, account['phone_number'], '', account['password'], "573203425072", message.message.encode('utf-8'), message_phone_whatsapp.id, image)
+            script, account['phone_number'], '', account['password'], phone.phone, message.message.encode('utf-8'), message_phone_whatsapp.id, image)
         ], shell=True)
     except Exception, e:
         transaction.rollback()
@@ -130,7 +130,7 @@ def launch_messege_whatsapp(account, cron_id):
         s.enter(6, 1, current_app.send_task, ('facenew.tasks.tasks.message_whatsapp', ({'phone_number': phone_number, 'password': password}, message)))
         s.enter(9, 1, current_app.send_task, ('facenew.tasks.tasks.message_whatsapp', ({'phone_number': phone_number, 'password': password}, message)))
         s.enter(12, 1, current_app.send_task, ('facenew.tasks.tasks.message_whatsapp', ({'phone_number': phone_number, 'password': password}, message)))
-        # s.enter(15, 1, current_app.send_task, ('facenew.tasks.tasks.message_whatsapp', ({'phone_number': phone_number, 'password': password}, message)))
+        s.enter(15, 1, current_app.send_task, ('facenew.tasks.tasks.message_whatsapp', ({'phone_number': phone_number, 'password': password}, message)))
         # s.enter(18, 1, current_app.send_task, ('facenew.tasks.tasks.message_whatsapp', ({'phone_number': phone_number, 'password': password}, message)))
         # s.enter(21, 1, current_app.send_task, ('facenew.tasks.tasks.message_whatsapp', ({'phone_number': phone_number, 'password': password}, message)))
         # s.enter(24, 1, current_app.send_task, ('facenew.tasks.tasks.message_whatsapp', ({'phone_number': phone_number, 'password': password}, message)))
